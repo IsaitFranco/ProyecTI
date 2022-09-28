52d9fc66d7aac1d1911d829a83cdd7b4ee1f1728
IsaitFranco <fabh02@gmail.com>
2022-09-28 09:30:12 -0500

    Pruebas Unitarias

---
+++ src/test/java/FileToolsTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileToolsTest {
+    @org.junit.jupiter.api.Test void ReadFile (){
+    }
+    @org.junit.jupiter.api.Test void WriteFile(){
+
+    }
+}
