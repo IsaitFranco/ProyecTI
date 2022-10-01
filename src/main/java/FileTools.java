77adc3930d4a9ee4492cf3180922ebfef93dfa90
IsaitFranco <fabh02@gmail.com>
2022-10-01 11:48:39 -0500

    Correcciones y mejoras

--- src/main/java/FileTools.java
+++ src/main/java/FileTools.java
@@ -1,25 +1,17 @@
 import java.io.*;
 import java.nio.file.Path;
-
 public class FileTools {
+   static String texto2 = "";
 
-   // String path = "";
-    FileReader fileReader = null;
-    BufferedReader bufferedReader = null;
-    FileWriter fileNew = null;
-    PrintWriter pw = null;
-    File rutaArchivo = null;
-    public String texto2 = "";
     // Metodo de lectura de archivo txt
-    public void readFile(String path) {
-
+    public static void readFile(String path) throws FileNotFoundException {
+        File rutaArchivo = new File(path);
+        FileReader fileReader = new FileReader(rutaArchivo);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        StringBuilder stringBuilder = new StringBuilder("");
+        String linea;
         try {
-            rutaArchivo = new File(path);
-            fileReader = new FileReader(rutaArchivo);
-            bufferedReader = new BufferedReader(fileReader);
-            StringBuilder stringBuilder = new StringBuilder("");
-            String linea;
-            while ((linea = bufferedReader.readLine()) != null) {//lee linea por linea, omitindo los saltos e linea
+             while ((linea = bufferedReader.readLine()) != null) {//lee linea por linea, omitindo los saltos e linea
                 stringBuilder.append(linea + "\n");
             }
             System.out.println("Contenido" + stringBuilder.toString());
@@ -28,24 +20,23 @@ public class FileTools {
             System.out.println("Excepcion leyendo archivo:" + e.getMessage());
         } finally {
             try {
-                if (fileReader != null)
-                    fileReader.close();
-                if (bufferedReader != null)
-                    bufferedReader.close();
+                if (fileReader != null){
+                    fileReader.close();}
+                if (bufferedReader != null){
+                    bufferedReader.close();}
             } catch (IOException e) {
                 System.out.println("Excepcion cerrado" + e.getMessage());
             }
         }
-
     }
-
 // Metodo de creacion de archivo txt
-    public void writeFile(String path2) {
-        rutaArchivo = new File(path2);
+    public static void writeFile(String path2) {
+        File rutaArchivo = new File(path2);
+        FileWriter fileNew = null;
         if (rutaArchivo.exists() == false) {
             try {
-                fileNew = new FileWriter(rutaArchivo);
-                pw = new PrintWriter(fileNew);
+               fileNew = new FileWriter(rutaArchivo);
+               PrintWriter pw = new PrintWriter(fileNew);
                 for (int i = 0; i < 10; i++)
                     pw.println("Linea" + i);
             } catch (Exception e) {
@@ -53,14 +44,13 @@ public class FileTools {
             } finally {
                 try {
                     // Nuevamente aprovechamos el finally  para asegurarnos que se cierra el fichero
-                    if (null != fileNew)
-                        fileNew.close();
+                    if (null != fileNew){
+                        fileNew.close();}
                     System.out.println("Se creo el archivo correctamente");
                 } catch (Exception e2) {
                     e2.printStackTrace();
                 }
             }
-
         } else {
             System.out.println("El archivo ya esta creado");
         }
